afda2d6ec6f8f35807dafc018640b17b6b1d5386
Izzatbek Pulatov <izzat.pulatov97@gmail.com>
2021-02-24 16:53:10 +0500

    update log

--- cmd/controller.go
+++ cmd/controller.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"cliTest/models"
-	email "cliTest/utils/email"
-	sms "cliTest/utils/sms"
+	email "cliTest/service/email"
+	sms "cliTest/service/sms"
+	"cliTest/utils/log"
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -13,7 +14,7 @@ import (
 func actions(cmd *cobra.Command, args []string) {
 
 	if userId < 1 || productId < 1 {
-		print("user id or product id is not supported", nil)
+		log.Info("user id or product id is not supported")
 		os.Exit(1)
 	}
 
@@ -28,12 +29,12 @@ func actions(cmd *cobra.Command, args []string) {
 func get() (models.User, models.Product) {
 	user := models.User{ID: userId}
 	if err := user.GetById(); err != nil {
-		print("user is not found", errors.Wrap(err, "get user by id"))
+		log.Error("user is not found", errors.Wrap(err, "get user by id"))
 		os.Exit(1)
 	}
 	product := models.Product{ID: productId}
 	if err := product.GetById(); err != nil {
-		print("product is not found", errors.Wrap(err, "get product by id"))
+		log.Error("product is not found", errors.Wrap(err, "get product by id"))
 		os.Exit(1)
 	}
 
@@ -43,9 +44,9 @@ func get() (models.User, models.Product) {
 func sendSms() {
 	user, product := get()
 	if err := sms.Send(user.PhoneNumber, fmt.Sprintf("Product: %s\nPrice: %d\n", product.Name, product.Price)); err == nil {
-		print("SMS Sent!", nil)
+		log.Info("SMS Sent!")
 	} else {
-		print("SMS sent failed", errors.Wrap(err, "sms sent"))
+		log.Error("SMS sent failed", errors.Wrap(err, "sms sent"))
 		os.Exit(1)
 	}
 }
@@ -54,9 +55,9 @@ func sendEmail() {
 	user, product := get()
 	if err := email.Send(user.Email,
 		fmt.Sprintf("Product: %s\nPrice: %d\n", product.Name, product.Price)); err == nil {
-		print("Email Sent!", nil)
+		log.Info("Email Sent!")
 	} else {
-		print("Email Sent!", errors.Wrap(err, "email sent"))
+		log.Error("Email Sent!", errors.Wrap(err, "email sent"))
 		os.Exit(1)
 	}
 
