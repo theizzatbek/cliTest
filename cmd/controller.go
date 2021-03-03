da1aeacbba5af14c8819135d98867380aae54e63
Izzatbek Pulatov <izzat.pulatov97@gmail.com>
2021-03-03 12:44:26 +0500

    some changes

--- cmd/controller.go
+++ cmd/controller.go
@@ -14,7 +14,7 @@ import (
 func actions(cmd *cobra.Command, args []string) {
 
 	if userId < 1 || productId < 1 {
-		log.Info("user id or product id is not supported")
+		log.Error(errors.New("user id or product id is not supported"))
 		os.Exit(1)
 	}
 
@@ -26,38 +26,49 @@ func actions(cmd *cobra.Command, args []string) {
 	}
 }
 
-func get() (models.User, models.Product) {
+func get() (models.User, string) {
+
+	var err error
+
+	defer func() {
+		if err != nil {
+
+			log.Error(err)
+			os.Exit(1)
+		}
+	}()
+
 	user := models.User{ID: userId}
-	if err := user.GetById(); err != nil {
-		log.Error("user is not found", errors.Wrap(err, "get user by id"))
-		os.Exit(1)
+	if err = user.GetById(); err != nil {
+		return models.User{}, ""
 	}
 	product := models.Product{ID: productId}
-	if err := product.GetById(); err != nil {
-		log.Error("product is not found", errors.Wrap(err, "get product by id"))
-		os.Exit(1)
+	if err = product.GetById(); err != nil {
+		return models.User{}, ""
 	}
 
-	return user, product
+	msg := fmt.Sprintf("Product: %s\nPrice: %d\n", product.Name, product.Price)
+
+	return user, msg
 }
 
 func sendSms() {
-	user, product := get()
-	if err := sms.Send(user.PhoneNumber, fmt.Sprintf("Product: %s\nPrice: %d\n", product.Name, product.Price)); err == nil {
+
+	user, msg := get()
+	if err := sms.Send(user.PhoneNumber, msg); err == nil {
 		log.Info("SMS Sent!")
 	} else {
-		log.Error("SMS sent failed", errors.Wrap(err, "sms sent"))
-		os.Exit(1)
+		return
 	}
 }
 
 func sendEmail() {
-	user, product := get()
-	if err := email.Send(user.Email,
-		fmt.Sprintf("Product: %s\nPrice: %d\n", product.Name, product.Price)); err == nil {
+	user, msg := get()
+
+	if err := email.Send(user.Email, msg); err == nil {
 		log.Info("Email Sent!")
 	} else {
-		log.Error("Email Sent!", errors.Wrap(err, "email sent"))
+		log.Error(errors.Wrap(err, "email sent failed"))
 		os.Exit(1)
 	}
 
--- utils/log/log.go
+++ utils/log/log.go
@@ -2,22 +2,36 @@ package log
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"os"
 	"time"
 )
+
+var file *os.File
 
 func Info(desc string) {
 	fmt.Println(desc)
 }
 
-func Error(desc string, err error) {
-	fmt.Println(desc)
-	f, _ := os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	defer f.Close()
-	fmt.Fprintf(f, "[%s]\t%s: %+v\n", time.Now().Format(time.RFC3339), desc, err)
+func Error(err error, args ...interface{}) {
+
+	fmt.Sprintf("[ERROR] %v", err)
+	//fmt.Println("ERR:", err.Error())
+	logToFile(err, args...)
 	//logrus.SetOutput(f)
 	//logrus.WithFields(logrus.Fields{
 	//	"desc": desc,
 	//}).Trace(err)
 
+}
+
+func init() {
+	file, _ = os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+}
+
+func logToFile(err error, args ...interface{}) {
+
+	//defer f.Close()
+	fmt.Fprintf(file, "[%s]\t \n%+v\n",
+		time.Now().Format(time.RFC3339), errors.Wrap(err, "[ERROR] "))
 }
